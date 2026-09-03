1439ad0ce417673e2181e557e10cd1c26b46fbc4
Nguyen-Xuan-Doan <doannx1151@ut.edu.vn>
2026-09-03 20:30:29 +0700

    Hoan thanh module Login va Profile - Nguoi 2

---
+++ app/Http/Middleware/CheckAdmin.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\Auth;
+
+class CheckAdmin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  Closure(Request): (Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if (Auth::check() && Auth::user()->role === 'admin') {
+            return $next($request);
+        }
+
+        abort(403, 'Bạn không có quyền truy cập trang này.');
+    }
+}
